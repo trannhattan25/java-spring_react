dddbd9e064f781f040c3931271b02a521b32241a
trannhattan25 <tanwaifu125@gmail.com>
2026-02-21 21:43:53 +0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { use, useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [users,setUsers] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/users")
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error("エラー発生:", error);
+      });
+  }, []);
 
+  return (
+    <div>
+      <h1>ユーザー一覧</h1>
+      <ul>
+        {users.map(user=>(
+          <li key={user.id}>{user.name}-{user.email}</li>
+        ))}
+        </ul> 
+    </div>
+  )
+}
 export default App;
